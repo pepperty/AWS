ea7dfb6a4a6a2f10c3eafa140c034259e83cdff2
Tiradate <114634898+Tiradate@users.noreply.github.com>
2023-09-01 12:46:26 +0700

    Create serial.py

---
+++ serial.py
@@ -0,0 +1,10 @@
+import serial, string
+
+output = " "
+ser = serial.Serial('/dev/ttyUSB0', 4800, 8, 'N', 1, timeout=1)
+while True:
+  print "----"
+  while output != "":
+    output = ser.readline()
+    print output
+  output = " "
