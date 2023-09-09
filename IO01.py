c422f9054fad0620589b7ab779f1c00b90c8d44a
Tiradate <114634898+Tiradate@users.noreply.github.com>
2023-09-09 16:26:57 +0700

    Add PJ code

--- IO01.py
+++ IO01.py
@@ -5,60 +5,96 @@ import time
 # Define GPIO to use on Pi
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO_TRIGGER = 23
-GPIO_ECHO = 24
+IO_EXC01 = 18
+IO_EXC02= 23
+IO_DET01 = 24
+IO_DET02 = 25
+IO_TRIG01= 8
+IO_TRIG02 = 7
+IO_REL01 = 12
+IO_REL02 = 16
 
+# ULtrasonic trigger time
 TRIGGER_TIME = 0.00001
-MAX_TIME = 0.004  # max time waiting for response in case something is missed
-GPIO.setup(GPIO_TRIGGER, GPIO.OUT)  # Trigger
-GPIO.setup(GPIO_ECHO, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Echo
+MAX_TIME = 0.05  # max time waiting for response in case something is missed
 
-GPIO.output(GPIO_TRIGGER, False)
+# GPIO.setup(IO_TRIG01, GPIO.OUT)  # Trigger
+# GPIO.setup(IO_EXC01, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Echo
+# GPIO.setup(IO_TRIG02, GPIO.OUT)  # Trigger
+# GPIO.setup(IO_EXC02, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Echo
 
-# This function measures a distance
+GPIO.setup(IO_EXC01, GPIO.OUT)
+GPIO.setup(IO_EXC02, GPIO.OUT)
+GPIO.setup(IO_DET01, GPIO.OUT)
+GPIO.setup(IO_DET02, GPIO.OUT)
+GPIO.setup(IO_TRIG01, GPIO.OUT)
+GPIO.setup(IO_TRIG02, GPIO.OUT)
+GPIO.setup(IO_REL01, GPIO.OUT)
+GPIO.setup(IO_REL02, GPIO.OUT)
+
+out  = 0 # test
+if out == 0:
+    GPIO.output(IO_EXC01, False)
+    GPIO.output(IO_EXC02, False)
+    GPIO.output(IO_DET01, False)
+    GPIO.output(IO_DET02, False)
+    GPIO.output(IO_TRIG01, False)
+    GPIO.output(IO_TRIG02, False)
+    GPIO.output(IO_REL01, False)
+    GPIO.output(IO_REL02, False)
+if out == 1:
+    GPIO.output(IO_EXC01, True)
+    GPIO.output(IO_EXC02, True)
+    GPIO.output(IO_DET01, True)
+    GPIO.output(IO_DET02, True)
+    GPIO.output(IO_TRIG01, True)
+    GPIO.output(IO_TRIG02, True)
+    GPIO.output(IO_REL01, True)
+    GPIO.output(IO_REL02, True)
 
 
-def measure():
-    # Pulse the trigger/echo line to initiate a measurement
-    GPIO.output(GPIO_TRIGGER, True)
-    time.sleep(TRIGGER_TIME)
-    GPIO.output(GPIO_TRIGGER, False)
+# # This function measures a distance
+# def measure():
+#     # Pulse the trigger/echo line to initiate a measurement
+#     GPIO.output(IO_TRIG01, True)
+#     time.sleep(TRIGGER_TIME)
+#     GPIO.output(IO_TRIG01, False)
 
-    # ensure start time is set in case of very quick return
-    start = time.time()
-    timeout = start + MAX_TIME
+#     # ensure start time is set in case of very quick return
+#     start = time.time()
+#     timeout = start + MAX_TIME
 
-    # set line to input to check for start of echo response
-    while GPIO.input(GPIO_ECHO) == 0 and start <= timeout:
-        start = time.time()
+#     # set line to input to check for start of echo response
+#     while GPIO.input(IO_EXC01) == 0 and start <= timeout:
+#         start = time.time()
 
-    if(start > timeout):
-        return -1
+#     if(start > timeout):
+#         return -1
 
-    stop = time.time()
-    timeout = stop + MAX_TIME
-    # Wait for end of echo response
-    while GPIO.input(GPIO_ECHO) == 1 and stop <= timeout:
-        stop = time.time()
+#     stop = time.time()
+#     timeout = stop + MAX_TIME
+#     # Wait for end of echo response
+#     while GPIO.input(IO_EXC01) == 1 and stop <= timeout:
+#         stop = time.time()
 
-    if(stop <= timeout):
-        elapsed = stop-start
-        distance = float(elapsed * 34300)/2.0
-    else:
-        return -1
-    return distance
+#     if(stop <= timeout):
+#         elapsed = stop-start
+#         distance = float(elapsed * 34300)/2.0
+#     else:
+#         return -1
+#     return distance
 
-print ("")
-if __name__ == '__main__':
-    try:
-        while True:
-            distance = measure()
-            if(distance > -1):
-                print("Measured Distance = %.1f cm" % distance)
-            else:
-                print("#")
-            time.sleep(0.5)
-        # Reset by pressing CTRL + C
-    except KeyboardInterrupt:
-        print("Measurement stopped by User")
-        GPIO.cleanup()
+# print ("")
+# if __name__ == '__main__':
+#     try:
+#         while True:
+#             distance = measure()
+#             if(distance > -1):
+#                 print("Measured Distance = %.1f cm" % distance)
+#             else:
+#                 print("#")
+#             time.sleep(0.5)
+#         # Reset by pressing CTRL + C
+#     except KeyboardInterrupt:
+#         print("Measurement stopped by User")
+#         GPIO.cleanup()
--- U2F2D2.py
+++ U2F2D2.py
@@ -5,10 +5,11 @@ import time
 # Define GPIO to use on Pi
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-IO_EXC01 = 18
-IO_EXC02= 23
-IO_DET01 = 24
-IO_DET02 = 25
+
+IO_DET01 = 18
+IO_DET02= 23
+IO_EXC01 = 24
+IO_EXC02 = 25
 IO_TRIG01= 8
 IO_TRIG02 = 7
 IO_REL01 = 12
@@ -18,83 +19,88 @@ IO_REL02 = 16
 TRIGGER_TIME = 0.00001
 MAX_TIME = 0.05  # max time waiting for response in case something is missed
 
-# GPIO.setup(IO_TRIG01, GPIO.OUT)  # Trigger
-# GPIO.setup(IO_EXC01, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Echo
-# GPIO.setup(IO_TRIG02, GPIO.OUT)  # Trigger
-# GPIO.setup(IO_EXC02, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Echo
-
-GPIO.setup(IO_EXC01, GPIO.OUT)
-GPIO.setup(IO_EXC02, GPIO.OUT)
-GPIO.setup(IO_DET01, GPIO.OUT)
-GPIO.setup(IO_DET02, GPIO.OUT)
+GPIO.setup(IO_DET01, GPIO.IN, pull_up_down=GPIO.PUD_Down)
+GPIO.setup(IO_DET02, GPIO.IN, pull_up_down=GPIO.PUD_Down)
+GPIO.setup(IO_EXC01, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(IO_EXC02, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(IO_TRIG01, GPIO.OUT)
 GPIO.setup(IO_TRIG02, GPIO.OUT)
 GPIO.setup(IO_REL01, GPIO.OUT)
 GPIO.setup(IO_REL02, GPIO.OUT)
 
-out  = 0 # test
-if out == 0:
-    GPIO.output(IO_EXC01, False)
-    GPIO.output(IO_EXC02, False)
-    GPIO.output(IO_DET01, False)
-    GPIO.output(IO_DET02, False)
-    GPIO.output(IO_TRIG01, False)
-    GPIO.output(IO_TRIG02, False)
-    GPIO.output(IO_REL01, False)
-    GPIO.output(IO_REL02, False)
-if out == 1:
-    GPIO.output(IO_EXC01, True)
-    GPIO.output(IO_EXC02, True)
-    GPIO.output(IO_DET01, True)
-    GPIO.output(IO_DET02, True)
-    GPIO.output(IO_TRIG01, True)
-    GPIO.output(IO_TRIG02, True)
-    GPIO.output(IO_REL01, True)
-    GPIO.output(IO_REL02, True)
+# This function measures a distance
+def measure(IO_TRIG,IO_EXC):
+    # Pulse the trigger/echo line to initiate a measurement
+    GPIO.output(IO_TRIG, True)
+    time.sleep(TRIGGER_TIME)
+    GPIO.output(IO_TRIG, False)
 
+    # ensure start time is set in case of very quick return
+    start = time.time()
+    timeout = start + MAX_TIME
 
-# # This function measures a distance
-# def measure():
-#     # Pulse the trigger/echo line to initiate a measurement
-#     GPIO.output(IO_TRIG01, True)
-#     time.sleep(TRIGGER_TIME)
-#     GPIO.output(IO_TRIG01, False)
+    # set line to input to check for start of echo response
+    while GPIO.input(IO_EXC) == 0 and start <= timeout:
+        start = time.time()
 
-#     # ensure start time is set in case of very quick return
-#     start = time.time()
-#     timeout = start + MAX_TIME
+    if(start > timeout):
+        return -1
 
-#     # set line to input to check for start of echo response
-#     while GPIO.input(IO_EXC01) == 0 and start <= timeout:
-#         start = time.time()
+    stop = time.time()
+    timeout = stop + MAX_TIME
+    # Wait for end of echo response
+    while GPIO.input(IO_EXC) == 1 and stop <= timeout:
+        stop = time.time()
 
-#     if(start > timeout):
-#         return -1
+    if(stop <= timeout):
+        elapsed = stop-start
+        distance = float(elapsed * 34300)/2.0
+    else:
+        return -1
+    return distance
 
-#     stop = time.time()
-#     timeout = stop + MAX_TIME
-#     # Wait for end of echo response
-#     while GPIO.input(IO_EXC01) == 1 and stop <= timeout:
-#         stop = time.time()
+if __name__ == '__main__':
+    try:
+        while True:
+            dist_A  = 0
+            dist_B  = 0
+            dist_A  = measure(IO_TRIG01,IO_EXC01)
+            dist_B  = measure(IO_TRIG02,IO_EXC02)
 
-#     if(stop <= timeout):
-#         elapsed = stop-start
-#         distance = float(elapsed * 34300)/2.0
-#     else:
-#         return -1
-#     return distance
+            if(dist_A > -1):
+                print("Measured Distance = %.1f cm" % dist_A)
+            else:
+                print("#UA")
+            if(dist_B > -1):
+                print("Measured Distance = %.1f cm" % dist_A)
+            else:
+                print("#UB")
+            time.sleep(0.1)
 
-# print ("")
-# if __name__ == '__main__':
-#     try:
-#         while True:
-#             distance = measure()
-#             if(distance > -1):
-#                 print("Measured Distance = %.1f cm" % distance)
-#             else:
-#                 print("#")
-#             time.sleep(0.5)
-#         # Reset by pressing CTRL + C
-#     except KeyboardInterrupt:
-#         print("Measurement stopped by User")
-#         GPIO.cleanup()
+            # Read the state of the GPIO pin
+            DET01_state = GPIO.input(IO_DET01)
+            DET02_state = GPIO.input(IO_DET02)
+
+            # Print the state (it should read LOW when the button is not pressed)
+            if DET01_state == GPIO.HIGH:
+                print("DET01 Detect")
+            else:
+                print("DET01 Non Detect")
+            if DET02_state == GPIO.HIGH:
+                print("DET02 Detect")
+            else:
+                print("DET02 Non Detect")
+
+            # Add a small delay to avoid rapid readings
+            time.sleep(0.1)
+
+            
+
+        # Reset by pressing CTRL + C
+    except KeyboardInterrupt:
+        print("Measurement stopped by User")
+        GPIO.cleanup()
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        GPIO.cleanup()
+    GPIO.cleanup()
